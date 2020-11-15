5577fef9e32b3d1a5ad922bf4751da4ac451dab9
Maksym Zaika <Onlyrobochaposhta@gmail.com>
2020-11-15 04:26:15 +0200

    fixed

--- src/js/sorchImagesFunction.js
+++ src/js/sorchImagesFunction.js
@@ -1,46 +1,59 @@
 const debounce = require('lodash.debounce');
+import animateScrollTo from 'animated-scroll-to';
 import templatesCard from '../templates/createCard.hbs';
 import ApiService from './apiService';
 const containerCard = document.querySelector('.gallery');
 const btnEl = document.querySelector('[data-action=load]');
-// const list = document.querySelector('.stylelist');
 
-const inputEl = document.querySelector('[name="query"]');
+const inputEl = document.querySelector('#search-form');
+const lol = document.querySelector('[name=query]');
 
-inputEl.addEventListener('input', debounce(onInputSearchImages, 500));
-btnEl.addEventListener('click', debounce(fetchCreateMarcupLoadMore, 300));
+inputEl.addEventListener('submit', onInputSearchImages);
+btnEl.addEventListener('click', fetchCreateMarcupLoadMore);
 
 const apiService = new ApiService();
-// lol();
 
 function onInputSearchImages(e) {
-  e.preventDefault();
-
   clearList();
-  if (e.target.value.length === 0) {
+  if (lol.value.length === 0) {
     btnEl.disabled = true;
+
     return;
   }
+  e.preventDefault();
 
   btnEl.disabled = false;
 
-  apiService.query = e.target.value;
+  apiService.query = lol.value;
   apiService.resetPage();
   fetchCreateMarcupLoadMore();
 }
-// inputEl.addEventListener('keydown', function (e) {
-//   if (e.code === 'Enter') {
-//     e.preventDefault();
-//   }
-// });
 
-async function fetchCreateMarcupLoadMore(e) {
+async function fetchCreateMarcupLoadMore() {
   try {
     const hits = await apiService.searchImages();
+    markup(hits);
+    const standartL = containerCard.children.length;
 
-    await markup(hits);
-
-    scroll();
+    // if (standartL !== 0) {
+    //   const getTop = standartL - 12;
+    //   const name = containerCard.children[`${getTop}`];
+    //   console.log(getTop);
+    //   window.scrollTo({
+    //     top: name,
+    //     left: 0,
+    //     behavior: 'smooth',
+    //   });
+    // }
+    const getTop = standartL - 12;
+    animateScrollTo(containerCard.children[`${getTop}`], {
+      speed: 500,
+      maxDuration: 3000,
+      verticalOffset: -20,
+    });
+    if (hits.length === 0) {
+      btnEl.disabled = true;
+    }
   } catch (error) {
     console.warn(error);
   }
@@ -50,26 +63,17 @@ function markup(data) {
   const cards = templatesCard(data);
   containerCard.insertAdjacentHTML('beforeend', cards);
 }
-
-function scroll() {
-  window.scrollTo({
-    top: selectedFrame.offsetTop,
-    left: 0,
-    behavior: 'smooth',
-  });
-}
-
 function clearList() {
   containerCard.innerHTML = '';
 }
 
-function lol() {
-  const list = document.querySelectorAll('.list-style');
-
-  if (list.length < 1) {
-    btnEl.disabled = true;
-  }
-}
+// function scroll(e) {
+//   window.scrollTo({
+//     top: e.offsetTop,
+//     left: 0,
+//     behavior: 'smooth',
+//   });
+// }
 
 // function onInputSearchImages(e) {
 //   // if (e.target.value.leanght === 0) {
